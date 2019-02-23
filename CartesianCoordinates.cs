ad75bad12e9d01c6cc7e74781ba7baf8887398cf
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-23 18:22:30 +0300

    add exception

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -8,24 +8,20 @@ namespace PadawansTask9
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
             List<Point> PointList = new List<Point>();
-            try
-            {
-                //Проверка range more 0
-                if (range <= 0)
-                {
-                    throw new ArgumentException();
-                }
-            }
-            catch (ArgumentException)
-            {
-
-            }
+            if (point.Equals(null))
+                throw new ArgumentNullException();
+            if (points == null)
+                throw new ArgumentNullException();
+            if (range <= 0)
+                throw new ArgumentException();
 
             for (int i = 0; i <= points.Length - 1; i++)
             {
                 if (Math.Abs(point.X - points[i].X) <= range
                     && Math.Abs(point.Y - points[i].Y) <= range)
                 {
+                    if (points[i].Equals(null))
+                        throw new ArgumentNullException();
                     PointList.Add(points[i]);
                 }
             }
