1dad36dfb0a85612c4de348ad117fd8f14d91d04
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-23 15:25:32 +0300

    Update CartesianCoordinates.cs

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -7,8 +7,30 @@ namespace PadawansTask9
     {
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
-            // put your code here
-            throw new NotImplementedException();
+            List<Point> PointList = new List<Point>();
+            try
+            {
+                //Проверка range more 0
+                if (range <= 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (ArgumentException)
+            {
+
+            }
+
+            for (int i = 0; i <= points.Length - 1; i++)
+            {
+                if (Math.Abs(point.X - points[i].X) <= range
+                    && Math.Abs(point.Y - points[i].Y) <= range)
+                {
+                    PointList.Add(points[i]);
+                }
+            }
+            return PointList;
+
         }
     }
 }
